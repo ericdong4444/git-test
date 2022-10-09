9b2ca7b1dd0e565728c528e5ff9dee908eb64dd2
eric <eric@atguigu.com>
2022-10-09 14:32:34 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author eric_dong
+ * @create 2022-10-09-13:52
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
